fc582aecd386e753b4b2dcbfcae8d07ff0e58033
Bartłomiej Tartanus <bartektartanus@gmail.com>
2019-07-13 17:15:01 +0200

    icons map and fromString method

--- tool/update.sh
+++ tool/update.sh
@@ -14,5 +14,8 @@ dart ./tool/generate_font.dart /tmp/icons.json
 dart ./tool/generate_example.dart /tmp/icons.json
 dartfmt -w ./lib/font_awesome_flutter.dart
 dartfmt -w ./example/lib/icons.dart
+dartfmt -w ./lib/font_awesome_flutter_map.dart
+
+
 
 rm /tmp/icons.json
