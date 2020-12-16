58ef28c6443f5d3d09c98b2ac9cd2a38b7cf9421
Michael Spiss <michaelspiss.dev@mailbox.org>
2020-12-16 19:05:26 +0100

    Fix null-safety for updated duotone icons

--- lib/src/icon_data.dart
+++ lib/src/icon_data.dart
@@ -54,16 +54,17 @@ class IconDataLight extends IconData {
 /// [IconData] for a font awesome duotone icon from a code point. Only works if
 /// duotone icons (pro) have been installed.
 ///
-/// Code points can be obtained from fontawesome.com. Each duotone icon has
-/// a primary [codePoint] and a [secondary]. Due to tree-shaking restraints,
-/// [secondary] cannot be the codepoint itself but has to be an [IconData]
-/// object.
+/// Code points can be obtained from fontawesome.com. Each duotone icon consists
+/// of a primary [codePoint] and a [secondary].
 class IconDataDuotone extends IconData {
-  final IconData secondary;
+  /// Secondary glyph of the duotone icon
+  ///
+  /// Due to tree-shaking restraints [secondary] cannot be the codepoint itself,
+  /// but has to be an [IconData] object.
+  final IconData? secondary;
 
-  const IconDataDuotone(int codePoint, {IconData secondary})
-      : this.secondary = secondary,
-        super(
+  const IconDataDuotone(int codePoint, {this.secondary})
+      : super(
           codePoint,
           fontFamily: 'FontAwesomeDuotone',
           fontPackage: 'font_awesome_flutter',
